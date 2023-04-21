10bb23653d02d43898b279c52df1619b2b2bf90d
A. Merlinov <merlinovandrej@gmail.com>
2023-04-21 18:55:08 +0000

    Update index.html and useApiData.js

--- src/hooks/useApiData.js
+++ src/hooks/useApiData.js
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useApiData() {
   const [states, setStates] = useState([]);
   const [lastUpdate, setLastUpdate] = useState([]);
   const [loading, setLoading] = useState(true);
-    
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://alerts.com.ua/api/states', {
+        const response = await axios.get("https://alerts.com.ua/api/states", {
           headers: {
-            'X-API-Key': 'e9cb5f274f9620b9dd5e5ce5807f59c5670e8af6',
+            "X-API-Key": "e9cb5f274f9620b9dd5e5ce5807f59c5670e8af6",
           },
         });
         setStates(response.data.states);
@@ -25,10 +25,10 @@ export default function useApiData() {
 
     fetchData();
 
-    console.log('Данные загружены!');
+    console.log("Данные загружены!");
     const intervalId = setInterval(() => {
       fetchData();
-      console.log('Обновлено!');
+      console.log("Обновлено!");
     }, 15000);
     return () => clearInterval(intervalId);
   }, [setStates, setLastUpdate]);
